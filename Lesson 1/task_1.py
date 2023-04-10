3c3f90fd21dd6778cafa0c670575b3b8c00c3de2
Tatiana Kozlova <soenam@mail.ru>
2023-04-10 22:08:54 +0300

    Add task 2 and utils

--- Lesson 1/task_1.py
+++ Lesson 1/task_1.py
@@ -1,11 +1,10 @@
+from utils import get_data_from_user
 # Найдите сумму цифр трехзначного числа
-def get_data_from_user():
-    return input()
 
 def main():
-    print("Введите трехзначное число")
-    data = get_data_from_user()
+    data = get_data_from_user("Введите трехзначное число")
 
     print(f'{data[0]} + {data[1]} + {data[2]} = {int(data[0]) + int(data[1]) + int(data[2])}')
 
-main()
+if __name__ == '__main__':
+    main()
---
+++ Lesson 1/utils.py
@@ -0,0 +1,3 @@
+def get_data_from_user(notification):
+    print(notification)
+    return input()
