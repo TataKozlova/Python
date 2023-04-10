5658ff92cc621b0c2786dcd42c2d85259f4d0490
Tatiana Kozlova <soenam@mail.ru>
2023-04-10 21:26:50 +0300

    Add task 1

--- Lesson 1/task_1.py
+++ Lesson 1/task_1.py
@@ -1,1 +1,11 @@
-# Найдите сумму цифр трехзначного числа 
+# Найдите сумму цифр трехзначного числа
+def get_data_from_user():
+    return input()
+
+def main():
+    print("Введите трехзначное число")
+    data = get_data_from_user()
+
+    print(f'{data[0]} + {data[1]} + {data[2]} = {int(data[0]) + int(data[1]) + int(data[2])}')
+
+main()
