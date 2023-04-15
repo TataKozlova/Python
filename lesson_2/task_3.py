7558712d9d9f02eb209efa19aab390e67d5aa7db
Tatiana Kozlova <soenam@mail.ru>
2023-04-15 21:24:33 +0300

    add task3

---
+++ lesson_2/task_3.py
@@ -0,0 +1,19 @@
+#Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+from utils import get_data_from_user
+from math import sqrt
+
+
+
+def main():
+    n = int(get_data_from_user("Введите число N"))
+    p = 1
+    while n > 2**p:
+        p = p + 1
+
+    for i in range(1,p):
+        print(f"2 в степени {i} = {2**i}, это меньше {n}")
+
+
+if __name__ == '__main__':
+    main()
